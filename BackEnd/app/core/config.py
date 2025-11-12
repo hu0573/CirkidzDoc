3d36e3e94346277aafd93050ac314ef14b54c74d
Ruiqing Hu <huruiqing0011@gmail.com>
2025-11-12 10:35:12 +1030

    chore: allow frontend cors from cloud host

--- BackEnd/app/core/config.py
+++ BackEnd/app/core/config.py
@@ -59,6 +59,7 @@ class Settings(BaseSettings):
         default=(
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "http://141.148.141.184:9004",
         )
     )
 
